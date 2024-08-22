7d748e07bafcd804940e15fef52abc05ae395fc1
Cadecraft <cadendotlee@gmail.com>
2024-08-22 12:04:40 -0700

    feat: wezterm size

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Wezterm config
--- R: v0.1.2; E: 2024/08/19
+-- R: v0.1.1; E: 2024/08/22
 
 -- Setup
 local wezterm = require 'wezterm'
@@ -8,6 +8,10 @@ local config = wezterm.config_builder()
 -- Shell
 config.default_prog = { 'powershell.exe' }
 
+-- Window
+config.initial_cols = 110;
+config.initial_rows = 30;
+
 -- Appearance: misc.
 config.color_scheme = 'iceberg-dark'
 config.font = wezterm.font('Consolas 7NF')
@@ -15,8 +19,8 @@ config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Appearance: OS specific
--- config.window_background_opacity = 0.8
--- config.win32_system_backdrop = 'Acrylic' -- For Windows 11
+config.window_background_opacity = 0.8
+config.win32_system_backdrop = 'Acrylic' -- For Windows 11
 
 -- Keys
 config.keys = {
