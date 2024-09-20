340a6ecc35b6e82cad83065566d1e56fa9767665
Cadecraft <cadendotlee@gmail.com>
2024-09-20 10:28:55 -0700

    feat: wezterm padding removal

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Wezterm config
--- R: v0.1.1; E: 2024/08/22
+-- R: v0.1.2; E: 2024/09/20
 
 -- Setup
 local wezterm = require 'wezterm'
@@ -17,6 +17,12 @@ config.color_scheme = 'iceberg-dark'
 config.font = wezterm.font('Consolas 7NF')
 config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = '0cell',
+	right = '0cell',
+	top = '0cell',
+	bottom = '0cell'
+}
 
 -- Appearance: OS specific
 config.window_background_opacity = 0.8
